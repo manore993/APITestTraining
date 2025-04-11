a126091415da986e84d07a3a581a368ba80c3dfa
Aishwarya MANORE <manore.aishwarya@gmail.com>
2025-04-11 17:42:58 +0200

    Write second test: test_get_all_requirements

--- test_squashdemo_apis/test_squashdemo_apis.py
+++ test_squashdemo_apis/test_squashdemo_apis.py
@@ -1,6 +1,6 @@
 import requests
 
-def test_get_all_requirements():
+def test_first_try():
 
     url = "https://demo.squashtest.org/squash/api/rest/latest/docs/api-documentation.html#_get_all_projects"
 
@@ -15,3 +15,27 @@ def test_get_all_requirements():
     assert response.status_code == 200
     assert response.headers.get("Content-Type") == "text/html"
     assert "Squash TM REST API" in response.text
+
+def test_get_all_requirements():
+    
+    url = "https://demo.squashtest.org/squash/api/rest/latest/requirements?size=2&page=50"
+
+    payload = {}
+    headers = {
+        "Cookie": 'JSESSIONID=60606E958927263DD68B61754ADE96ED'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    
+    data = response.json()
+    page_data = data.get("page", {})
+    size = page_data.get("size")
+    total_elements = page_data.get("totalElements")
+    total_pages = page_data.get("totalPages")
+    number = page_data.get("number")
+
+    assert response.status_code == 200
+    assert size == 2
+    assert total_elements == 253
+    assert total_pages == 127
+    assert number == 50
